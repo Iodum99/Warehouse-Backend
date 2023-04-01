2cfddf4b3783cc6718aaa6ab60d292a47fe4b03e
Milica <milicavujanic30@gmail.com>
2023-04-01 20:38:41 +0200

    Initial project and database configuration

---
+++ warehouse/src/main/java/com/example/warehouse/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.warehouse.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/test")
+@CrossOrigin
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<?> test(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
